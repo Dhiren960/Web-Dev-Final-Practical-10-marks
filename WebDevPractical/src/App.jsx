69368c10bb823a184c1cb781010f4f3e6128a984
Dhiren960 <d879475@gmail.com>
2026-05-04 10:31:54 +0530

    first commit

--- WebDevPractical/src/App.jsx
+++ WebDevPractical/src/App.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react" 
 
 function App() {
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]) 
+  const [page, setPage] = useState(1) 
 
-  const limit = 4;
+  const limit = 4 
 
   useEffect(() => {
-    const skip = (page - 1) * limit;
+    const skip = (page - 1) * limit 
 
     fetch(`https://dummyjson.com/products?limit=4&skip=${skip}`)
       .then((res) => res.json())
-      .then((res) => setData(res.products));
-  }, [page]);
+      .then((res) => setData(res.products)) 
+  }, [page]) 
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -21,7 +21,7 @@ function App() {
       {data.map((item) => (
         <div key={item.id}>
           <img src={item.thumbnail} alt="" width="150" />
-          <p>{item.title}</p>
+          
         </div>
       ))}
 
@@ -33,7 +33,7 @@ function App() {
         Next
       </button>
     </div>
-  );
+  ) 
 }
 
-export default App;
+export default App 
